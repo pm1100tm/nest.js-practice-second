c5f2d681f9463d78d5fae23cc2e79a84274a97d2
dev-swd <tladnjsen@gmail.com>
2021-09-22 15:39:47 +0900

    Add auth modle

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { BoardModule } from './board/board.module';
 import { typeORMConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardModule, AuthModule],
   controllers: [AppController],
   providers: [],
 })
